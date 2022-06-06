11406133425e24e0c23345a508efc79079863e17
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-06 17:59:14 +0300

    Added time calculation api route

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PalindromeController;
+use App\Http\Controllers\TimeCalculationController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/getpalindrome', [PalindromeController::class, 'palindromeFinder'])->name("palindrome-finder");
-
+Route::get('/getseconds', [TimeCalculationController::class, 'secondsCalculators'])->name("seconds-calculator");
