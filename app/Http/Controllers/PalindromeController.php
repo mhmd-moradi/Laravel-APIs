01ffbbd533b1de145dfda82a753e59686f990c03
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-06 17:46:40 +0300

    Added palindrome route

--- app/Http/Controllers/PalindromeController.php
+++ app/Http/Controllers/PalindromeController.php
@@ -10,15 +10,24 @@ class PalindromeController extends Controller
         $strings = array("civic", "test", "radar", "not", "apple", "racecar");
 
         //find palindrome function
-        function Palindrome($string){ 
+        function palindrome($string){ 
             if (strrev($string) == $string){ 
                 return 1; 
             }
             else{
                 return 0;
             }
-        } 
+        }
 
-        
+        $palindromes = 0;
+        foreach ($strings as $string) {
+            if(palindrome($string))
+                $palindromes += 1;
+        }
+
+        return response()->json([
+            "status" => "Success",
+            "number_of_palindromes" => $palindromes
+        ], 200);
     }
 }
