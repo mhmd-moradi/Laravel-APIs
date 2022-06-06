5684549749ab0da34cb155a3a28f9bb2a20e4fb4
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-06 17:39:12 +0300

    Added palindromeFinder function

--- app/Http/Controllers/PalindromeController.php
+++ app/Http/Controllers/PalindromeController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class PalindromeController extends Controller
 {
     public function palindromeFinder(){
-       
+        $strings = array("civic", "test", "radar", "not", "apple", "racecar");
+
+        //find palindrome function
+        function Palindrome($string){ 
+            if (strrev($string) == $string){ 
+                return 1; 
+            }
+            else{
+                return 0;
+            }
+        } 
+
+        
     }
 }
