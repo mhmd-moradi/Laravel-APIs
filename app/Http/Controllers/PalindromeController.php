5f0a2e9a582203d3b68adaed70c09e5d609d772a
mhmd-moradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-06-06 17:38:41 +0300

    Created PalindromeController

---
+++ app/Http/Controllers/PalindromeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PalindromeController extends Controller
+{
+    public function palindromeFinder(){
+       
+    }
+}
